73b91740568db3fc8288bf9b54ee2489ae7c8be3
Ryan Brisentine <brisentine.ryan@gmail.com>
2018-01-14 20:54:38 -0600

    updates to about.html, connected stylesheet, product_controller changes

--- js/product.js
+++ js/product.js
@@ -1,5 +1,7 @@
 
-var prod = [
+//Create a Factory (I decided to use an array to iterate between examples.)
+/*
+var product = [
 
     {
     
@@ -45,6 +47,7 @@ var prod = [
         
     },
 
+
     {
     
         name: 'Mobile Tracker',
@@ -88,4 +91,70 @@ var prod = [
         
     }
 
-]
+]*/
+var products = [
+	{
+		img_src: '../images/cropped_images/BroBucks.jpg',
+		img_alt: 'Two dudes holding sacks of money.',
+		name: 'BroBucks',
+		description: 'Financial Management for Bros',
+		price: '$1.99',
+		link: 'URL'
+	},
+	{
+		img_src: '../images/cropped_images/BetterBro.jpg',
+		img_alt: 'Bro and his Belle enjoying a moment.',
+		name: 'BetterBro',
+		description: 'Dating Advice for Bros',
+		price: '$1.99',
+		link: 'URL'
+	},
+	{
+		img_src: '../images/cropped_images/BroMove.jpg',
+		img_alt: 'Bro driving a luxury car.',
+		name: 'BroMove',
+		description: 'Ridesharing, Uber, Lyft',
+		price: '$1.99',
+		link: 'URL'
+	},
+	{
+		img_src: '../images/cropped_images/BroGigs.jpg',
+		img_alt: 'Bros working at a coffee roaster',
+		name: 'BroGigs',
+		description: 'Jobs for Bros',
+		price: '$1.99',
+		link: 'URL'
+	},
+	{
+		img_src: '../images/cropped_images/BroFit.jpg',
+		img_alt: 'Tan bros leaving the gym to do laundry.',
+		name: 'BroFit',
+		description: 'Gym buddy and gym finder',
+		price: '$1.99',
+		link: 'URL'
+	},
+	{
+		img_src: '../images/cropped_images/BroJoe.jpg',
+		img_alt: 'Bro having an espresso.',
+		name: 'BroJoe',
+		description: 'Artisan coffee shop finder, and in store directory. Coffee jargon available as in-app purchase',
+		price: '$1.99',
+		link: 'URL'
+	},
+	{
+		img_src: '../images/cropped_images/BroBrew.jpg',
+		img_alt: 'Group of bros enjoying a beer.',
+		name: 'BroBrew',
+		description: 'Duh, self explanatory',
+		price: '$1.99',
+		link: 'URL'
+	},
+	{
+		img_src: '../images/cropped_images/BroTix.jpg',
+		img_alt: 'Bros enjoying a concert.',
+		name: 'BroTix',
+		description: 'Find tickets with your Bros',
+		price: '$1.99',
+		link: 'URL'
+	},
+];
